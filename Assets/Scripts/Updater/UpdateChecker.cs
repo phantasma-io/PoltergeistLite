6cd31632661a37b6f9b66320f94411413ec2be1b
alexmpa <alexander.paukonen@gmail.com>
2025-02-27 19:05:59 -0300

    Bump version

--- Assets/Scripts/Updater/UpdateChecker.cs
+++ Assets/Scripts/Updater/UpdateChecker.cs
@@ -8,7 +8,7 @@ public class UpdateChecker : MonoBehaviour
 {
     public string githubOwner = "phantasma-io"; // Your GitHub username
     public string githubRepo = "PoltergeistLite"; // Your repository name
-    public string currentVersion = "1.3.0"; // Current version of your game
+    public string currentVersion = "1.3.1"; // Current version of your game
 
     private const string GITHUB_RELEASES_URL = "https://github.com/";
     private static string URL = "";
